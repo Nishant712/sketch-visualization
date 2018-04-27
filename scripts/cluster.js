a45c353d468e57973bad44558dfc83b8e99c35e8
Nishant Barma <nishantbarma@hotmail.com>
2018-04-27 17:24:15 +0000

    updated

--- scripts/cluster.js
+++ scripts/cluster.js
@@ -105,7 +105,7 @@ function createBubbleChart(error, dataset) {
 	  
 	var svg,
 	  circles,
-	  circleSize = { min: 10, max: 80 };
+	  circleSize = { min: 10, max: 60 };
 	  
 	var circleRadiusScale = d3.scaleSqrt()
 	.domain(populationExtent)
